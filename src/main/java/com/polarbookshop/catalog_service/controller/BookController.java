c3a30e298dcfc2bb6d3b8658bfb7203187c5878d
erickknsilva <erick.nsilva100@gmail.com>
2025-05-30 18:03:05 -0300

    Adicionando SpringBoot Actuator para monitoramentos de metricas e observabilidade, expondo os endpoints do actuator via yml para checkar a saude, metrics e liberando as sondas/probes para pode utilizar o livenessProbe e readinessProbe no Kubernetes para buscar no container desse applicativo. Tambem foi incluindo o Grafana mais sem acoplamento de dependencia ele está configurado via Docker, então podemos remover o grafana a qualquer hora. Grafana é a plataforma escolhida para monitoramento.

--- src/main/java/com/polarbookshop/catalog_service/controller/BookController.java
+++ src/main/java/com/polarbookshop/catalog_service/controller/BookController.java
@@ -4,6 +4,8 @@ import com.polarbookshop.catalog_service.domain.BookService;
 import com.polarbookshop.catalog_service.dto.BookRequest;
 import com.polarbookshop.catalog_service.dto.BookResponse;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("books")
 public class BookController {
 
+    private static Logger log = LoggerFactory.getLogger(BookController.class);
     private final BookService bookService;
 
     public BookController(BookService bookService) {
@@ -20,17 +23,20 @@ public class BookController {
 
     @GetMapping
     public Iterable<BookResponse> get() {
+        log.info("Fetching the list of books in the catalog.");
         return bookService.viewBookList();
     }
 
     @GetMapping("{isbn}")
     public BookResponse getByIsbn(@PathVariable String isbn) {
+        log.info("Fetching the book with ISBN {} from the catalog", isbn);
         return bookService.viewBookDetails(isbn);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public BookResponse post(@Valid @RequestBody BookRequest request) {
+        log.info("Adding a new book to the catalog with ISBN {}", request.isbn());
         return bookService.addBookToCatalog(request);
     }
 
@@ -38,11 +44,13 @@ public class BookController {
     @DeleteMapping("{isbn}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable String isbn) {
+        log.info("Deleting book with ISBN {}", isbn);
         bookService.removeBookFromCatalog(isbn);
     }
 
     @PutMapping("{isbn}")
     public BookResponse put(@PathVariable String isbn, @Valid @RequestBody BookRequest request) {
+        log.info("Updating book with ISBN {}", isbn);
         return bookService.editBookDetails(isbn, request);
     }
 
